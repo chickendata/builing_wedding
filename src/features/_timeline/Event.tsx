1d64497dfc3845e7236fc4405079bff4e6c2fa65
phuongNV <nvp.1312.2001@gmail.com>
2024-06-01 22:37:47 +0700

    fix bug lan 2 va chua design loading

--- src/features/_timeline/Event.tsx
+++ src/features/_timeline/Event.tsx
@@ -91,7 +91,12 @@ const Event = () => {
       }
     );
     setIsTriggerZip(true);
-    setZipFilePath(zipResponse.data);
+    console.log(zipResponse.data);
+    const pathZip: string = zipResponse.data.replace(
+      "/var/www/build_futurelove",
+      "photo.fakewedding.online"
+    );
+    setZipFilePath(pathZip);
     nProgress.done();
   };
 
@@ -113,7 +118,6 @@ const Event = () => {
   };
 
   const handleMakeVideo = async () => {
-    setIsMakeVideo(true);
     nProgress.start();
     const response = await axios.get(
       "https://databaseswap.mangasocial.online/get/list_2_image/id_image_swap_all_id_sk",
@@ -151,10 +155,11 @@ const Event = () => {
       "/var/www/build_futurelove",
       "photo.fakewedding.online"
     );
+    setIsMakeVideo(true);
     setPathVideo(linkDataVideo);
     nProgress.done();
   };
-  console.log(isMakeVideo);
+  console.log(pathVideo);
 
   return (
     <>
@@ -423,34 +428,36 @@ const Event = () => {
             <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-black/50">
               <div className="relative bg-white shadow-lg rounded p-6 flex flex-col items-center md:translate-y-[-20%] w-[58%] h-[58%] rounded-[49px] border-solid border-[rgba(0,157,196,0.5)] border-4">
                 <div className="md:py-[30px]">
-                  <svg
-                    width="77"
-                    height="70"
-                    viewBox="0 0 77 70"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="animate-bounce mx-auto md:mb-11 mb-2"
-                  >
-                    <path
-                      d="M73 35C73 39.071 72.1076 43.1021 70.3738 46.8632C68.6401 50.6243 66.0988 54.0417 62.8952 56.9203C59.6916 59.7989 55.8883 62.0824 51.7026 63.6403C47.5168 65.1982 43.0306 66 38.5 66C33.9694 66 29.4832 65.1982 25.2974 63.6403C21.1117 62.0824 17.3084 59.7989 14.1048 56.9203C10.9012 54.0417 8.35994 50.6243 6.62616 46.8632C4.89237 43.1021 4 39.071 4 35C4 30.929 4.89237 26.8979 6.62616 23.1368C8.35995 19.3757 10.9012 15.9583 14.1048 13.0797C17.3084 10.2011 21.1117 7.91763 25.2974 6.35973C29.4832 4.80184 33.9694 4 38.5 4C43.0306 4 47.5169 4.80184 51.7026 6.35974C55.8883 7.91763 59.6916 10.2011 62.8952 13.0797C66.0988 15.9583 68.6401 19.3757 70.3738 23.1368C72.1076 26.8979 73 30.929 73 35L73 35Z"
-                      stroke="url(#paint0_angular_216_1969)"
-                      stroke-width="8"
-                      stroke-linecap="round"
-                    />
-                    <defs>
-                      <radialGradient
-                        id="paint0_angular_216_1969"
-                        cx="0"
-                        cy="0"
-                        r="1"
-                        gradientUnits="userSpaceOnUse"
-                        gradientTransform="translate(38.5 35) rotate(90) scale(31 34.5)"
-                      >
-                        <stop stop-color="white" />
-                        <stop offset="1" stop-color="#2D2D2D" />
-                      </radialGradient>
-                    </defs>
-                  </svg>
+                  <div className="md:mb-11 mb-2">
+                    <svg
+                      width="77"
+                      height="70"
+                      viewBox="0 0 77 70"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mx-auto animate-[spin_2s_linear_infinite]"
+                    >
+                      <path
+                        d="M73 35C73 39.071 72.1076 43.1021 70.3738 46.8632C68.6401 50.6243 66.0988 54.0417 62.8952 56.9203C59.6916 59.7989 55.8883 62.0824 51.7026 63.6403C47.5168 65.1982 43.0306 66 38.5 66C33.9694 66 29.4832 65.1982 25.2974 63.6403C21.1117 62.0824 17.3084 59.7989 14.1048 56.9203C10.9012 54.0417 8.35994 50.6243 6.62616 46.8632C4.89237 43.1021 4 39.071 4 35C4 30.929 4.89237 26.8979 6.62616 23.1368C8.35995 19.3757 10.9012 15.9583 14.1048 13.0797C17.3084 10.2011 21.1117 7.91763 25.2974 6.35973C29.4832 4.80184 33.9694 4 38.5 4C43.0306 4 47.5169 4.80184 51.7026 6.35974C55.8883 7.91763 59.6916 10.2011 62.8952 13.0797C66.0988 15.9583 68.6401 19.3757 70.3738 23.1368C72.1076 26.8979 73 30.929 73 35L73 35Z"
+                        stroke="url(#paint0_angular_216_1969)"
+                        stroke-width="8"
+                        stroke-linecap="round"
+                      />
+                      <defs>
+                        <radialGradient
+                          id="paint0_angular_216_1969"
+                          cx="0"
+                          cy="0"
+                          r="1"
+                          gradientUnits="userSpaceOnUse"
+                          gradientTransform="translate(38.5 35) rotate(90) scale(31 34.5)"
+                        >
+                          <stop stop-color="white" />
+                          <stop offset="1" stop-color="#2D2D2D" />
+                        </radialGradient>
+                      </defs>
+                    </svg>
+                  </div>
                   <div className="text-center">
                     <ProgressPercentage
                       onComplete={() => {
